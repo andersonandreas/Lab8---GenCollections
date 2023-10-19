49cbf9f7b8f1d9b9ecb667ab753619360c2a7315
Andreas A <adde1990@live.se>
2023-10-19 15:16:55 +0200

    update

--- GenericCollections/Program.cs
+++ GenericCollections/Program.cs
@@ -2,14 +2,12 @@
 {
     internal class Program
     {
+        // Andreas Anderson .NET23
 
         static void Main(string[] args)
         {
 
-
-
-            //------------------ Del 1 – Stack ------------------
-
+            //------------------------------------ Part 1– Stack ------------------------------------
 
             Employee employee1 = new Employee(1001, "Andreas", "male", 70000);
             Employee employee2 = new Employee(1002, "Daniel", "male", 25800);
@@ -19,14 +17,15 @@
 
 
             Stack<Employee> employees = new Stack<Employee>();
+
+            // adding the poped Employees
+            Stack<Employee> popedEmployees = new Stack<Employee>();
+
             employees.Push(employee1);
             employees.Push(employee2);
             employees.Push(employee3);
             employees.Push(employee4);
             employees.Push(employee5);
-
-            // i dont know this one maybe save in the same Stack?
-            Stack<Employee> employees2 = new Stack<Employee>();
 
 
             foreach (Employee emp in employees)
@@ -45,7 +44,7 @@
                 Console.WriteLine($"{emp.Id} - {emp.Name} - {emp.Gender} - {emp.Salary}");
                 Console.WriteLine($"Items left in the stack = {employees.Count}");
 
-                employees2.Push(emp);
+                popedEmployees.Push(emp);
 
             }
 
@@ -54,84 +53,81 @@
             Console.WriteLine("Retrive using Peek method");
 
 
-            if (employees2.Count > 0)
+            if (popedEmployees.Count > 0)
             {
                 for (int i = 0; i < 2; i++)
                 {
-                    Employee emp = employees2.Peek();
+                    Employee emp = popedEmployees.Peek();
                     Console.WriteLine($"{emp.Id} - {emp.Name} - {emp.Gender} - {emp.Salary}");
-                    Console.WriteLine($"Items left in the stack = {employees2.Count}");
+                    Console.WriteLine($"Items left in the stack = {popedEmployees.Count}");
                 }
             }
 
             Console.WriteLine("---------------------------------");
 
-            if (employees2.Contains(employee3))
+            if (popedEmployees.Contains(employee3))
             {
                 Console.WriteLine("Emp3 is in the stack");
             }
 
 
-            // ------------------ Del 2 – List ------------------
+            // ------------------------------------ Part 2 – List ------------------------------------
 
-            //Employee employee1 = new Employee(1001, "Andreas", "male", 70000);
-            //Employee employee2 = new Employee(1002, "Daniel", "male", 25800);
-            //Employee employee3 = new Employee(1003, "Sofia", "female", 43000);
-            //Employee employee4 = new Employee(1004, "Linnea", "female", 59300);
-            //Employee employee5 = new Employee(1005, "Linus", "male", 385000);
+            Console.WriteLine("\n-------------Part 2----------------\n");
 
-            //List<Employee> EmployeeList = new List<Employee>();
+            Employee employee6 = new Employee(1001, "Andreas", "male", 70000);
+            Employee employee7 = new Employee(1002, "Daniel", "male", 25800);
+            Employee employee8 = new Employee(1003, "Sofia", "female", 43000);
+            Employee employee9 = new Employee(1004, "Linnea", "female", 59300);
+            Employee employee10 = new Employee(1005, "Linus", "male", 385000);
 
-            //EmployeeList.Add(employee1);
-            //EmployeeList.Add(employee2);
-            //EmployeeList.Add(employee3);
-            //EmployeeList.Add(employee4);
-            //EmployeeList.Add(employee5);
+            List<Employee> EmployeeList = new List<Employee>();
 
-
-
-            //if (EmployeeList.Contains(employee2))
-            //{
-            //    Console.WriteLine("Employee2 object exists in the list\n");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Employee2 object does not exist in the list");
-            //}
-
-            //// find the first male in the list
-            //Employee? finded = EmployeeList.Find(emp => emp.Gender == "male");
-            //Console.WriteLine("The first employee that is male:");
-
-            //if (finded != null)
-            //{
-            //    Console.WriteLine($"ID = {finded.Id}, Name = {finded.Name}, Gender = {finded.Gender}, Salary = {finded.Salary} \n");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("No match in the list \n");
-            //}
-
-
-            //Console.WriteLine("All the males in the list:");
-
-
-            //// list with all the items that match my search in this case "male"
-            //List<Employee> results = EmployeeList.FindAll(GenderSearch);
-
-            //foreach (Employee emp in results)
-            //{
-
-            //    Console.WriteLine($"ID = {emp.Id}, Name = {emp.Name}, Gender = {emp.Gender}, Salary = {emp.Salary}");
-            //}
+            EmployeeList.Add(employee6);
+            EmployeeList.Add(employee7);
+            EmployeeList.Add(employee8);
+            EmployeeList.Add(employee9);
+            EmployeeList.Add(employee10);
 
 
 
+            if (EmployeeList.Contains(employee7))
+            {
+                Console.WriteLine("Employee7 object exists in the list\n");
+            }
+            else
+            {
+                Console.WriteLine("Employee7 object does not exist in the list");
+            }
 
-            //static bool GenderSearch(Employee Emp)
-            //{
-            //    return Emp.Gender == "male";
-            //}
+            // find the first male in the list
+            Employee? finded = EmployeeList.Find(emp => emp.Gender == "male");
+            Console.WriteLine("The first employee that is male:");
+
+            if (finded != null)
+            {
+                Console.WriteLine($"ID = {finded.Id}, Name = {finded.Name}, Gender = {finded.Gender}, Salary = {finded.Salary} \n");
+            }
+            else
+            {
+                Console.WriteLine("No match in the list \n");
+            }
+
+
+            Console.WriteLine("All the males in the list:");
+
+
+            // Find all the males in the list
+            List<Employee> results = EmployeeList.FindAll(emp => emp.Gender == "male");
+
+            foreach (Employee emp in results)
+            {
+
+                Console.WriteLine($"ID = {emp.Id}, Name = {emp.Name}, Gender = {emp.Gender}, Salary = {emp.Salary}");
+            }
+
+
+
 
 
         }
